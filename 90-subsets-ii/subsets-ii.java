86f619b441ee7dd483afac0de705ee59c013a36c
Aashishkishor <aashishkishor4@gmail.com>
2026-08-22 10:27:45 +0530

    Time: 2 ms (99.73%) | Memory: 45.2 MB (42.34%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,18 +1,36 @@
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        dfs(nums, 0, new ArrayList<>(), res);
-        return res;
+
+    void fun(List<Integer> input,List<Integer> output,List<List<Integer>> ans){
+        ans.add(new ArrayList<>(output));
+
+        if(input.size()==0) return;
+
+        for(int i=0;i<input.size();i++){
+            List<Integer> ip=new ArrayList<>(input);
+            List<Integer> op=new ArrayList<>(output);
+
+            if(i>0 && ip.get(i)==ip.get(i-1)) continue;
+
+            op.add(ip.get(i));
+            ip.subList(0,i+1).clear();
+
+            fun(ip,op,ans);
+        }
     }
 
-    private void dfs(int[] nums, int start, List<Integer> path, List<List<Integer>> res) {
-        res.add(new ArrayList<>(path));
-        for (int i = start; i < nums.length; i++) {
-            if (i > start && nums[i] == nums[i - 1]) continue;
-            path.add(nums[i]);
-            dfs(nums, i + 1, path, res);
-            path.remove(path.size() - 1);
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> input=new ArrayList<>();
+        List<Integer> output=new ArrayList<>();
+
+        for(int a:nums){
+            input.add(a);
         }
+
+        Collections.sort(input);
+
+        fun(input,output,ans);
+
+        return ans;
     }
 }
