dc47f84542f9ac5001c6fd31366e36801a726d09
Aashishkishor <aashishkishor4@gmail.com>
2026-09-25 23:31:11 +0530

    Time: 6 ms (96.54%) | Memory: 161.9 MB (56.40%) - LeetSync

---
+++ 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minPathCost(int[][] grid, int[][] moveCost) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        int[] dp = new int[n];
+        
+        for (int j = 0; j < n; j++) {
+            dp[j] = grid[0][j];
+        }
+        
+        for (int i = 0; i < m - 1; i++) {
+            int[] nextDp = new int[n];
+            java.util.Arrays.fill(nextDp, Integer.MAX_VALUE);
+            
+            for (int j = 0; j < n; j++) {
+                int val = grid[i][j];
+                for (int nextCol = 0; nextCol < n; nextCol++) {
+                    int cost = dp[j] + moveCost[val][nextCol] + grid[i + 1][nextCol];
+                    nextDp[nextCol] = Math.min(nextDp[nextCol], cost);
+                }
+            }
+            dp = nextDp;
+        }
+        
+        int minCost = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            minCost = Math.min(minCost, dp[j]);
+        }
+        
+        return minCost;
+        
+    }
+}
